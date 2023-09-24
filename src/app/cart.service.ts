23f398d4fb7eaba102343caebb888e996f4efab1
Shackeyb <131311411+Shackeyb@users.noreply.github.com>
2023-09-24 10:10:09 -0400

    1 file modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -7,8 +7,14 @@ import { Injectable } from '@angular/core';
 })
 export class CartService {
   items: Product[] = [];
+  getShippingPrices() {
+    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
+  }
 /* . . . */
-
+  constructor(
+    private http: HttpClient
+  ) {}
+  /* . . . */
   addToCart(product: Product) {
     this.items.push(product);
   }
